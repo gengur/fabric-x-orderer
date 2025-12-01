dd191a45aa0074658303281c387d9bd587b91fea
May.Buzaglo <May.Buzaglo@ibm.com>
2025-12-01 16:52:35 +0200

    import configtxlator from fabric-x-common
    Signed-off-by: May.Buzaglo <May.Buzaglo@ibm.com>

--- config/generate/config_block_gen.go
+++ config/generate/config_block_gen.go
@@ -13,15 +13,13 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
-	"net"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 
 	"github.com/hyperledger/fabric-protos-go-apiv2/common"
-	"github.com/hyperledger/fabric-x-common/internaltools/configtxgen"
-	"github.com/hyperledger/fabric-x-common/internaltools/configtxgen/genesisconfig"
+	"github.com/hyperledger/fabric-x-common/tools/configtxgen"
+	genesisconfig "github.com/hyperledger/fabric-x-common/tools/configtxgen"
 	"github.com/hyperledger/fabric-x-orderer/config"
 	"github.com/pkg/errors"
 )
@@ -56,11 +54,8 @@ func CreateGenesisBlock(blockDir string, baseDir string, sharedConfigYaml *confi
 func CreateProfile(dir string, sharedConfigYaml *config.SharedConfigYaml, sharedConfigPath string, sampleConfigPath string) (*genesisconfig.Profile, error) {
 	// collect relevant shared configuration
 	var consenterMapping []*genesisconfig.Consenter
-	var routerAndAssemblerEndpoints []string
-	for _, party := range sharedConfigYaml.PartiesConfig {
-		routerAndAssemblerEndpoints = append(routerAndAssemblerEndpoints, net.JoinHostPort(party.RouterConfig.Host, strconv.Itoa(int(party.RouterConfig.Port))))
-		routerAndAssemblerEndpoints = append(routerAndAssemblerEndpoints, net.JoinHostPort(party.AssemblerConfig.Host, strconv.Itoa(int(party.AssemblerConfig.Port))))
 
+	for _, party := range sharedConfigYaml.PartiesConfig {
 		consenter := &genesisconfig.Consenter{
 			ID:            uint32(party.PartyID),
 			Host:          party.ConsenterConfig.Host,
@@ -93,7 +88,7 @@ func CreateProfile(dir string, sharedConfigYaml *config.SharedConfigYaml, shared
 		org.ID = fmt.Sprintf("org%d", i+1)
 		org.Name = fmt.Sprintf("org%d", i+1)
 		org.MSPDir = filepath.Join(dir, "crypto", "ordererOrganizations", fmt.Sprintf("org%d", i+1), "msp")
-		org.OrdererEndpoints = routerAndAssemblerEndpoints
+		// TODO: org.OrdererEndpoints in the new format
 		// Update policy rules to use correct org names
 		for _, policy := range org.Policies {
 			policy.Rule = strings.ReplaceAll(policy.Rule, "SampleOrg", fmt.Sprintf("org%d", i+1))
