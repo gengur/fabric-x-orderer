9c551c0020b558b07ac6ccc0a578038ccc22ffa2
Genady Gurevich <genadyg@il.ibm.com>
2025-12-09 12:46:53 +0200

    send config update test moved to the node/router
    Signed-off-by: Genady Gurevich <genadyg@il.ibm.com>

--- test/config_disseminate_test.go
+++ test/config_disseminate_test.go
@@ -8,7 +8,6 @@ package test
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -16,110 +15,27 @@ import (
 	"time"
 
 	"github.com/hyperledger/fabric-protos-go-apiv2/common"
-	"github.com/hyperledger/fabric-protos-go-apiv2/msp"
-	"github.com/hyperledger/fabric-x-common/common/util"
 	"github.com/hyperledger/fabric-x-orderer/common/configstore"
 	"github.com/hyperledger/fabric-x-orderer/common/tools/armageddon"
 	"github.com/hyperledger/fabric-x-orderer/node/consensus/configrequest/mocks"
-	"github.com/hyperledger/fabric-x-orderer/node/crypto"
 	"github.com/hyperledger/fabric-x-orderer/testutil"
 	"github.com/hyperledger/fabric-x-orderer/testutil/client"
+	"github.com/hyperledger/fabric-x-orderer/testutil/fabric/configutil"
 	"github.com/onsi/gomega/gexec"
 	"github.com/stretchr/testify/assert"
-	"google.golang.org/protobuf/proto"
 
 	"github.com/hyperledger/fabric-x-common/protoutil"
 	policyMocks "github.com/hyperledger/fabric-x-orderer/common/policy/mocks"
 	"github.com/hyperledger/fabric-x-orderer/common/types"
 	"github.com/hyperledger/fabric-x-orderer/common/utils"
-	fabricx_config "github.com/hyperledger/fabric-x-orderer/config"
 	"github.com/hyperledger/fabric-x-orderer/node/comm/tlsgen"
 	"github.com/hyperledger/fabric-x-orderer/node/config"
 	"github.com/hyperledger/fabric-x-orderer/node/consensus/state"
 	"github.com/hyperledger/fabric-x-orderer/node/ledger"
 	protos "github.com/hyperledger/fabric-x-orderer/node/protos/comm"
-	"github.com/hyperledger/fabric-x-orderer/testutil/stub"
 	"github.com/hyperledger/fabric-x-orderer/testutil/tx"
 	"github.com/stretchr/testify/require"
 )
-
-// TestRouterSendConfigUpdateToConsenterStub tests the end-to-end flow of sending a configuration
-// update transaction through the router to a consenter stub.
-func TestRouterSendConfigUpdateToConsenterStub(t *testing.T) {
-	// 1. Compile arma
-	armaBinaryPath, err := gexec.BuildWithEnvironment("github.com/hyperledger/fabric-x-orderer/cmd/arma", []string{"GOPRIVATE=" + os.Getenv("GOPRIVATE")})
-	defer gexec.CleanupBuildArtifacts()
-	require.NoError(t, err)
-	require.NotNil(t, armaBinaryPath)
-
-	// 2. Create a temporary directory for the test.
-	dir, err := os.MkdirTemp("", t.Name())
-	require.NoError(t, err)
-	defer os.RemoveAll(dir)
-
-	numOfParties := 1
-	numOfShards := 1
-	submittingParty := 1
-
-	// 3. Create a config YAML file in the temporary directory.
-	configPath := filepath.Join(dir, "config.yaml")
-	netInfo := testutil.CreateNetwork(t, configPath, numOfParties, numOfShards, "TLS", "none")
-	require.NoError(t, err)
-
-	// 4. Generate the config files in the temporary directory using the armageddon generate command.
-	armageddon.NewCLI().Run([]string{"generate", "--config", configPath, "--output", dir})
-
-	configStoreDir := t.TempDir()
-	defer os.RemoveAll(configStoreDir)
-
-	// 5. Launch the batcher node stub
-	batcher := stub.NewStubBatcherFromConfig(t, configStoreDir, filepath.Join(dir, "config", "party1", "local_config_batcher1.yaml"), netInfo["Party13batcher1"].Listener)
-	batcher.Start()
-	defer batcher.Stop()
-
-	// 6. Launch the router node
-	readyChan := make(chan struct{}, 1)
-
-	routerNodeInfo := netInfo["Party14router"]
-	routerNodeConfigPath := filepath.Join(dir, "config", "party1", "local_config_router.yaml")
-	localConfig, _, err := fabricx_config.LoadLocalConfig(routerNodeConfigPath)
-	require.NoError(t, err)
-
-	localConfig.NodeLocalConfig.FileStore.Path = configStoreDir
-	localConfig.NodeLocalConfig.GeneralConfig.ClientSignatureVerificationRequired = true
-	utils.WriteToYAML(localConfig.NodeLocalConfig, routerNodeConfigPath)
-
-	testutil.RunArmaNodes(t, dir, armaBinaryPath, readyChan, map[string]*testutil.ArmaNodeInfo{"Party14router": routerNodeInfo})
-	testutil.WaitReady(t, readyChan, 1, 10)
-
-	// 7. Launch the consenter node stub
-	consenterStub := stub.NewStubConsenterFromConfig(t, configStoreDir, filepath.Join(dir, "config", "party1", "local_config_consenter.yaml"), netInfo["Party11consensus"].Listener)
-	consenterStub.Start()
-	defer consenterStub.Stop()
-
-	// 8. Create a broadcast client
-	uc, err := testutil.GetUserConfig(dir, 1)
-	assert.NoError(t, err)
-	assert.NotNil(t, uc)
-
-	broadcastClient := client.NewBroadcastTxClient(uc, 10*time.Second)
-	defer broadcastClient.Stop()
-
-	// 9. Prepare a Config TX, i.e. an envelope signed by an admin of org1
-	// the envelope.Payload contains marshaled bytes of configUpdateEnvelope, which is an envelope with Header.Type = HeaderType_CONFIG_UPDATE, signed by majority of admins
-	// Create the config transaction
-	genesisBlockPath := filepath.Join(dir, "bootstrap/bootstrap.block")
-	env := createConfigTX(t, dir, numOfParties, genesisBlockPath, submittingParty)
-	require.NotNil(t, env)
-
-	// 10. Send the config tx
-	err = broadcastClient.SendTx(env)
-	require.ErrorContains(t, err, "INTERNAL_SERVER_ERROR, Info: dummy submit config")
-
-	require.Eventually(t, func() bool {
-		return consenterStub.ReceivedMessageCount() == 1
-	}, 10*time.Second, 100*time.Millisecond)
-}
 
 func TestConfigDisseminate(t *testing.T) {
 	ca, err := tlsgen.NewCA()
@@ -331,7 +247,7 @@ func TestConfigTXDisseminationWithVerification(t *testing.T) {
 	// Create the config transaction
 	submittingParty := 1
 	genesisBlockPath := filepath.Join(dir, "bootstrap/bootstrap.block")
-	env := createConfigTX(t, dir, numOfParties, genesisBlockPath, submittingParty)
+	env := configutil.CreateConfigTX(t, dir, numOfParties, genesisBlockPath, submittingParty)
 	require.NotNil(t, env)
 
 	// Send the config tx
@@ -401,92 +317,4 @@ func TestConfigTXDisseminationWithVerification(t *testing.T) {
 			return consensusLedgerCount == 2
 		}, 60*time.Second, 100*time.Millisecond)
 	}
-}
-
-func CreateSigner(privateKeyPath string) (*crypto.ECDSASigner, error) {
-	// Read the private key
-	keyBytes, err := os.ReadFile(privateKeyPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read private key, err: %v", err)
-	}
-
-	// Create a ECDSA Singer
-	privateKey, err := tx.CreateECDSAPrivateKey(keyBytes)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create ECDSA Signer, err: %v", err)
-	}
-
-	return (*crypto.ECDSASigner)(privateKey), nil
-}
-
-func createConfigTX(t *testing.T, dir string, numOfParties int, genesisBlockPath string, submittingParty int) *common.Envelope {
-	// Create ConfigUpdateBytes
-	configUpdatePbBytes := CreateConfigUpdate(t, dir, genesisBlockPath)
-	require.NotNil(t, configUpdatePbBytes)
-
-	// create ConfigUpdateEnvelope
-	configUpdateEnvelope := &common.ConfigUpdateEnvelope{
-		ConfigUpdate: configUpdatePbBytes,
-		Signatures:   []*common.ConfigSignature{},
-	}
-
-	// sign with majority admins (for 4 parties the majority is 3)
-	for i := 0; i < (numOfParties/2)+1; i++ {
-		// Read admin of organization i
-		adminSigner, adminCertBytes, err := createCertAndSigner(dir, i+1)
-		require.NoError(t, err)
-		require.NotNil(t, adminSigner)
-		require.NotNil(t, adminCertBytes)
-
-		sId := &msp.SerializedIdentity{
-			Mspid:   fmt.Sprintf("org%d", i+1),
-			IdBytes: adminCertBytes,
-		}
-
-		sigHeader, err := protoutil.NewSignatureHeader(adminSigner)
-		require.NoError(t, err)
-
-		sigHeader.Creator = protoutil.MarshalOrPanic(sId)
-
-		configSig := &common.ConfigSignature{
-			SignatureHeader: protoutil.MarshalOrPanic(sigHeader),
-		}
-		configSig.Signature, err = adminSigner.Sign(util.ConcatenateBytes(configSig.SignatureHeader, configUpdateEnvelope.ConfigUpdate))
-		require.NoError(t, err)
-
-		configUpdateEnvelope.Signatures = append(configUpdateEnvelope.Signatures, configSig)
-	}
-
-	configUpdateEnvelopeBytes, err := proto.Marshal(configUpdateEnvelope)
-	require.NoError(t, err)
-
-	// Wrap the ConfigUpdateEnvelope with an Envelope signed by the admin of the submitting party
-	submittingAdminSigner, submittingAdminCert, err := createCertAndSigner(dir, submittingParty)
-	require.NoError(t, err)
-	require.NotNil(t, submittingAdminSigner)
-	require.NotNil(t, submittingAdminCert)
-
-	payload := tx.CreatePayloadWithConfigUpdate(configUpdateEnvelopeBytes, submittingAdminCert, fmt.Sprintf("org%d", submittingParty))
-	require.NotNil(t, payload)
-	env, err := tx.CreateSignedEnvelope(payload, submittingAdminSigner)
-	require.NoError(t, err)
-	require.NotNil(t, env)
-
-	return env
-}
-
-func createCertAndSigner(dir string, submittingParty int) (*crypto.ECDSASigner, []byte, error) {
-	keyPath := filepath.Join(dir, "crypto", "ordererOrganizations", fmt.Sprintf("org%d", submittingParty), "users", "admin", "msp", "keystore", "priv_sk")
-	submittingAdminSigner, err := CreateSigner(keyPath)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed creating a signer, err: %s", err)
-	}
-
-	certPath := filepath.Join(dir, "crypto", "ordererOrganizations", fmt.Sprintf("org%d", submittingParty), "users", "admin", "msp", "signcerts", fmt.Sprintf("Admin@Org%d-cert.pem", submittingParty))
-	submittingAdminCert, err := os.ReadFile(certPath)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed creating a certificate, err: %s", err)
-	}
-
-	return submittingAdminSigner, submittingAdminCert, nil
 }
